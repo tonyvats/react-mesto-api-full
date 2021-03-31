8e093428fea48d2a12a22780f48f1c3ae69d002f
Anton Vatc <yndx-vats@yandex.ru>
2021-03-31 21:03:23 +0300

    fix: интеграция на удаленном сервере

--- backend/app.js
+++ backend/app.js
@@ -75,7 +75,7 @@ app.use('/', router);
 app.use(errorLogger);
 
 app.use((err, req, res, next) => {
-  // console.log(err);
+  console.log(err);
   const { statusCode = 500, message } = err;
   res.status(statusCode).send({ message: statusCode === 500 ? 'На сервере произошла ошибка' : message });
 });
