bfe4bdb04ea93e41d0959773418fcde84d927f9f
Anton Vatc <yndx-vats@yandex.ru>
2021-03-31 17:35:48 +0300

    fix: интеграция на удаленном сервере

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,6 +1,6 @@
 // export const BASE_URL = 'https://auth.nomoreparties.co';
 // export const BASE_URL = 'http://localhost:3000';
-export const BASE_URL = 'http://vatc.nomoredomains.club';
+export const BASE_URL = 'http://vatc.nomoredomains.icu';
 
 
 const checkResponse = (response) => response.ok
