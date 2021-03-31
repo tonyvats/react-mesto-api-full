6733bc3fd611eb054fef936e0c74f4ef1306123f
Anton Vatc <yndx-vats@yandex.ru>
2021-03-31 03:18:47 +0300

    fix: интеграция на удаленном сервере

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -142,7 +142,7 @@ class Api {
 const api = new Api({
     // url: "https://mesto.nomoreparties.co/v1/cohort-18/",
     // url: "http://localhost:3000/",
-    url: "http://vatc.nomoredomains.icu/",
+    url: "http://api.vatc.nomoredomains.club/",
     // headers: {
     //     "Content-type": "application/json",
     // },
